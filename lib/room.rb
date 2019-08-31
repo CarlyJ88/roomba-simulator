9f3f69e3e607772cfc73bf43abebaaf967e7ac77
Carly <carly.jenkinson65@googlemail.com>
2019-08-31 13:46:29 +0100

    room has a dimension

---
+++ lib/room.rb
@@ -0,0 +1,14 @@
+require 'roomba'
+
+class Room
+
+  def initialize(dimensions)
+    @dimensions = dimensions
+  end
+
+  def dimensions
+    @dimensions
+  end
+
+end
+
---
+++ spec/room_spec.rb
@@ -0,0 +1,10 @@
+require 'room'
+require 'roomba'
+
+describe Room do
+  it 'has a small room' do
+    room = Room.new({ :x => 5, :y => 5 })
+    expect(room.dimensions).to eq ({ :x => 5, :y => 5 })
+  end
+
+end
