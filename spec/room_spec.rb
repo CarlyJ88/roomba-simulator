ff84f0b64c474f6a3027e4add454ecdea12f9bb3
Carly <carly.jenkinson65@googlemail.com>
2019-08-31 20:47:53 +0100

    Room has two patches of dirt.

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -10,4 +10,10 @@ describe Room do
     room = Room.new({ :x => 5, :y => 5 })
     expect(room.add_dirt({:x => 1, :y => 3})).to eq ([{:x => 1, :y => 3}])
   end
+
+  it 'has two patches of dirt' do
+    room = Room.new({ :x => 5, :y => 5 })
+    room.add_dirt({:x => 1, :y => 3})
+    expect(room.add_dirt({:x => 3, :y => 4})).to eq ([{:x => 1, :y => 3}, {:x => 3, :y => 4}])
+  end
 end
